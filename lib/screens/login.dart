57ad74168b170f55017e02a274440e20934068aa
Rodrigo Souza de Menezes <rsmenezes1@ipes.se.gov.br>
2024-07-17 16:32:49 -0300

    Alterando tela de login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -15,19 +15,17 @@ class Login extends StatelessWidget {
           color: Color.fromRGBO(0, 51, 153, 1),
         ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             const SizedBox(
               height: 80,
             ),
-            const Padding(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "Login",
-                style: TextStyle(color: Colors.white, fontSize: 40),
-              ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child:
+                  Image.asset(width: 205, height: 82, 'lib/assets/image.png'),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Expanded(
@@ -39,17 +37,88 @@ class Login extends StatelessWidget {
                       topRight: Radius.circular(60)),
                 ),
                 child: Padding(
-                  padding: EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(30),
                   child: Column(
                     children: <Widget>[
+                      const SizedBox(
+                        height: 80,
+                      ),
                       Container(
-                        padding: EdgeInsets.all(20),
+                        padding: const EdgeInsets.all(20),
                         decoration: BoxDecoration(
-                          boxShadow: [
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
                             BoxShadow(
-                              color: Color.fromRGBO(225, 95, 27, .3),
+                              color: Color.fromRGBO(0, 51, 153, 1),
+                              blurRadius: 20,
+                              offset: Offset(
+                                0,
+                                10,
+                              ),
                             ),
                           ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Email",
+                                    hintStyle: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                    border: InputBorder.none,
+                                    icon: Icon(Icons.email)),
+                              ),
+                            ),
+                            Container(
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Senha",
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                  border: InputBorder.none,
+                                  icon: Icon(Icons.remove_red_eye_sharp),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: const Color.fromRGBO(0, 51, 153, 1),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Entrar',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
                       )
                     ],
